67d35f25385b2a8fac69c567157981b8e8f83a41
jackie123-dotcom <115568922+jackie123-dotcom@users.noreply.github.com>
2026-04-11 22:49:27 -0700

    submission for HOS01A - Jackie

---
+++ Module01/assignment_1.py
@@ -0,0 +1,24 @@
+print("\nAugmented Operations of int and int\n")
+a = 10
+b = 5
+
+a += b # equivalent to a = a + b
+print("a=", a, "type(a):", type(a))
+
+a -= b # equivalent to a = a - b
+print("a=", a, "type(a):", type(a))
+
+a *= b # equivalent to a = a * b
+print("a=", a, "type(a):", type(a))
+
+a /= b # equivalent to a = a / b
+print("a=", a, "type(a):", type(a))
+
+a %= b # equivalent to a = a % b
+print("a=", a, "type(a):", type(a))
+
+a **= b # equivalent to a = a ** b
+print("a=", a, "type(a):", type(a))
+
+a //= b # equivalent to a = a // b
+print("a=", a, "type(a):", type(a))
---
+++ Module01/data_conversion.py
@@ -0,0 +1,5 @@
+stock_price = 10
+print("Please enter the number of stocks to buy: ")
+#print('Your total price is $ '+ stock_price * input())
+#print('Your total price is $ '+ str(stock_price * int(input())))
+print('Your total price is $ '+ str(stock_price * float(input())))
